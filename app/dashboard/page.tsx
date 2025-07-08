47e2953b7b262ffc743657bf05c8b9cecdf0a58d
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-07-08 13:14:43 +0530

    added databse postgress sql and orm prisma

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
-export default functon dashboardPage (){
+export default function DashboardRoute(){
     return(
         <div>
+            <h1>hello from dashboard </h1>
         </div>
     )
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,40 @@
-import Image from "next/image";
+import { prisma } from "./utils/db";
 
+async function getData() {
+  const data = await prisma.Postly.findMany({
+    select: {
+      title:true,
+      content: true,
+      imageUrl: true,
+      authorImage: true,
+      authorName: true,
+      id: true,
+      createdAt: true,
+      authorId: true,
+      updatedAt :true
 
-export default function Home() {
+    },
+  });
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
-    <div>
-      app
-      
-      
+   <div className="py-6">
+    <h1 className="text-6xl font-bold tracking-tight mb-8">Latest Post</h1>
+
+
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {data.map((item)=>(
+        <h1 key={item.title}>{item.title}</h1>
+      )
+)}
+
     </div>
+
+   </div>
   );
 }
