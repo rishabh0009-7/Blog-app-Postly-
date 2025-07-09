f17f4a2297c8b9a4c2adcb7b4b2d8e12bbcf9eb7
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-07-09 09:25:02 +0530

    showing data on dahsboard page which we created

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,26 +1,52 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
-export default async function DashboardRoute(){
-    
-    const {isAuthenticated } = getKindeServerSession()
-    const user = await isAuthenticated()
+import { prisma } from "../utils/db";
 
-    if(!user ){
-        redirect("/api/auth/register");
-    }
-    return(
-        <div>
-            <div className="flex items-center justify-between mb-4">
-                <h2 className="text-xl font-medium ">Your Blog Articles</h2>
+async function getData(userId: string) {
+  const data = await prisma.postly.findMany({
+    where: {
+      authorId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
+  return data;
+}
 
-        <Link className={buttonVariants()} href="/dashboard/create">
-          Create Post
-        </Link>
-            </div>
+export default async function DashboardRoute() {
+  const { getUser} = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    redirect("/api/auth/register");
+  }
+// why we use get user instead of isauthenticated coz ye boolean dega but we want user id 
+  const data = await getData(user.id);
+
+  return (
+    <div>
+    <div className="flex items-center justify-between mb-4">
+      <h2 className="text-xl font-medium">Your Blog Articles</h2>
+
+      <Link className={buttonVariants()} href="/dashboard/create">
+        Create Post
+      </Link>
+    </div>
+
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {data.map((item) => (
+        // <BlogPostCard data={item} key={item.id} />
+        <div key = {item.id}>
+          <h1>{item.title}</h1>
         </div>
-    )
+      ))}
+    </div>
+  </div>
+    
+  );
 
-} 
+}
